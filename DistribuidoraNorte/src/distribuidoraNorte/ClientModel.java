f1e23b9a9741eb5f3a371d2d87bd1c33acbce4c2
fmollea <fmollea@gmail.com>
2017-03-23 08:44:20 -0300

    add client model and main class

---
+++ DistribuidoraNorte/src/distribuidoraNorte/ClientModel.java
@@ -0,0 +1,60 @@
+package distribuidoraNorte;
+
+public class ClientModel {
+
+    private String name;
+    private String lastName;
+    private String address;
+    private Integer numAddress;
+    
+    //methods
+    public ClientModel(){
+        name = "";
+        lastName = "";
+        address = "";
+        numAddress = 0;
+    }
+    
+    public ClientModel(String n, String l, String a, Integer na){
+        name = n;
+        lastName = l;
+        address = a;
+        numAddress = na;
+    }
+    
+    //sets
+    
+    public void setName(String n){
+        name = n;
+    }
+    
+    public void setLastName(String l){
+        lastName = l;
+    }
+    
+    public void setAddress(String a){
+        address = a;
+    }
+    
+    public void setNumAddress(Integer n){
+        numAddress = n;
+    }
+    
+    //gets
+    
+    public String getName(){
+        return name;
+    }
+    
+    public String getLastName(){
+        return lastName;
+    }
+    
+    public String getAddress(){
+        return address;
+    }
+    
+    public Integer getNumAddress(){
+        return numAddress;
+    }
+}
---
+++ DistribuidoraNorte/src/distribuidoraNorte/Main.java
@@ -0,0 +1,10 @@
+
+package distribuidoraNorte;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+    
+}
