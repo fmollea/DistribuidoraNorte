cddbfa400ec802188c86a968963334e114cddce1
fmollea <fmollea@gmail.com>
2017-05-06 10:11:19 -0300

    DDBB schema

---
+++ DistribuidoraNorte/src/dao/schema.sql
@@ -0,0 +1,99 @@
+CREATE DATABASE distributor;
+USE distrubutor;
+
+CREATE TABLE client(
+	code VARCHAR(25) NOT NULL PRIMARY KEY,
+	name VARCHAR(25),
+	lastName VARCHAR(25),
+	address VARCHAR(25),
+	numAddress INT(10),
+	isActive INT(10)
+)ENGINE = INNODB;
+
+
+CREATE TABLE provider(
+	name VARCHAR(25) NOT NULL PRIMARY KEY,
+	city VARCHAR(25),
+	address VARCHAR(25),
+	numAddress INT(10),
+	numPhone VARCHAR(25),
+	email VARCHAR(25),
+	area VARCHAR(25)
+)ENGINE = INNODB;
+
+
+CREATE TABLE product(
+	code VARCHAR(25) NOT NULL PRIMARY KEY,
+	description VARCHAR(50),
+	stock INT(10),
+	isActive INT(10),
+	cost FLOAT,
+	price FLOAT,
+	area VARCHAR(25),
+	nameProvider VARCHAR(25),
+	INDEX (nameProvider),
+	FOREIGN KEY (nameProvider) REFERENCES provider(name)
+	ON DELETE SET NULL ON UPDATE CASCADE
+)ENGINE = INNODB;
+
+
+CREATE TABLE buy(
+	code VARCHAR(25) NOT NULL PRIMARY KEY,
+	cant INT(10),
+	cost FLOAT,
+	buyDate DATE,
+	FOREIGN KEY (code) REFERENCES product(code)
+	ON DELETE CASCADE ON UPDATE CASCADE
+)ENGINE = INNODB;
+
+CREATE TABLE sell(
+	numberSell INT NOT NULL PRIMARY KEY,
+	clientCode VARCHAR(25) NOT NULL,
+	sellDate DATE,
+	total FLOAT,
+	FOREIGN KEY (clientCode) REFERENCES client(code)
+	ON DELETE CASCADE ON UPDATE CASCADE
+)ENGINE = INNODB;
+
+-- clientPhone table is a list of client's phones
+CREATE TABLE clientPhone(
+	code VARCHAR(25) NOT NULL,
+	numberPhone VARCHAR(25) NOT NULL,
+	PRIMARY KEY (code, numberPhone),
+	FOREIGN KEY (code) REFERENCES client(code)
+	ON DELETE CASCADE ON UPDATE CASCADE
+)ENGINE = INNODB;
+
+-- providerPhone table is a list of provider's phones
+CREATE TABLE providerPhone(
+	name VARCHAR(25) NOT NULL,
+	numberPhone VARCHAR(25) NOT NULL,
+	PRIMARY KEY (name, numberPhone),
+	FOREIGN KEY (name) REFERENCES provider(name)
+	ON DELETE CASCADE ON UPDATE CASCADE
+)ENGINE = INNODB;
+
+
+-- sellProduct table is a list of products that was sold in sell table 
+CREATE TABLE sellProduct(
+	numberSell INT NOT NULL,
+	codeProduct VARCHAR(25),
+	cant INT,
+	PRIMARY KEY (numberSell, codeProduct),
+	FOREIGN KEY (numberSell) REFERENCES sell(numberSell)
+	ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (codeProduct) REFERENCES product(code)
+	ON DELETE CASCADE ON UPDATE CASCADE
+)ENGINE = INNODB;
+
+
+-- Triggers definition
+-- When the user buy a product then update the product stock and if the price of product is change, update it
+CREATE TRIGGER fixProduct_AfterBuy AFTER INSERT ON buy
+	FOR EACH ROW
+	   UPDATE product SET cost = NEW.cost, stock = stock + NEW.cant;
+
+-- When the user sell a product then update the product stock
+CREATE TRIGGER fixProduct_AfterSell AFTER INSERT ON sellProduct
+	FOR EACH ROW
+  	   UPDATE product SET stock = stock - NEW.cant;
