0dd44cd50175e858217f7eebbc9ce4b2898dbf25
fmollea <fmollea@gmail.com>
2017-05-08 16:27:17 -0300

    interface productDao

---
+++ DistribuidoraNorte/src/dao/ProductDao.java
@@ -0,0 +1,39 @@
+package dao;
+
+import distribuidoraNorte.ProductModel;
+import java.util.ArrayList;
+
+public interface ProductDao {
+    
+    public void addProductDao(ProductModel product);
+    
+    public void updateProducttDao(ProductModel product);
+    
+    public void activeProductDao(ProductModel product);
+    
+    public void deleteProductDao(ProductModel client);
+    
+    //search product
+    public ProductModel searchProductByCodeDao(String code);
+   
+    //search product for area
+    public ArrayList<ProductModel> searchProductByAreaDao(String area);
+    
+    //search all products that (product.stock < = stock)
+    public ArrayList<ProductModel> searchProductByStockDao(Integer stock);
+    
+    public ArrayList<ProductModel> searchProductByProviderDao(String name);
+    
+    
+    //diferent List of Product
+    public ArrayList<ProductModel> listOfProductDao();
+    
+    public ArrayList<ProductModel> listOfActiveProductDao();
+    
+    public ArrayList<ProductModel> listOfNotActiveProductDao();
+    
+    //list of product by more seller
+    public ArrayList<ProductModel> listOfSellProductDao();
+    
+   
+}
